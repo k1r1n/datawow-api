2ecd6a15961e4a19b39b4df6693d922ae8e54767
.S h i r a n a i <boybrandla@hotmail.com>
2025-05-31 12:03:47 +0700

    add endpoint create, get all concert

--- src/concerts/concerts.controller.ts
+++ src/concerts/concerts.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ConcertsService } from './concerts.service';
+import { CreateConcertDto } from './dto/create-concert.dto';
 
 @Controller('concerts')
-export class ConcertsController {}
+export class ConcertsController {
+  constructor(private readonly concertsService: ConcertsService) {}
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  create(@Body() createConcertDto: CreateConcertDto) {
+    return this.concertsService.create(createConcertDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.concertsService.findAll();
+  }
+}
--- src/concerts/concerts.service.ts
+++ src/concerts/concerts.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Concert } from './interfaces/concert.interface';
+import { CreateConcertDto } from './dto/create-concert.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class ConcertsService {}
+export class ConcertsService {
+  private readonly concerts: Concert[] = [];
+
+  create(createConcertDto: CreateConcertDto): Concert {
+    const newConcert: Concert = {
+      id: uuidv4(),
+      ...createConcertDto,
+      seatsAvailable: createConcertDto.seatTotal,
+    };
+    this.concerts.push(newConcert);
+    return newConcert;
+  }
+
+  findAll(): Concert[] {
+    return this.concerts;
+  }
+}
